996c348cca13fd7172ab71a60ddc7af7448c6f1e
yohan <yohanfernando@gmail.com>
2013-09-10 19:58:09 +0100

    Added the CRUD sql and formatted

--- app/models/POICategory.scala
+++ app/models/POICategory.scala
@@ -1,26 +1,18 @@
 package models
 
-import play.api.Play.current
-import anorm.SqlParser._
-import anorm._
 import play.api.db._
-
+import play.api.Play.current
+import anorm._
+import anorm.SqlParser._
 
 /**
- * Created with IntelliJ IDEA.
+ * Poi Category object
+ *
  * User: yfernan
- * Date: 11/08/2013
- * Time: 00:10
- * To change this template use File | Settings | File Templates.
  */
 case class PoiCategory(id: Long, name: String, imagePath: String)
 
-
 object PoiCategory {
-
-
-
-
 
   // -- Parsers
 
@@ -28,43 +20,47 @@ object PoiCategory {
    * Parse a User from a ResultSet
    */
   val simple = {
-    get[Long]("poi_categories.id") ~
-      get[String]("poi_categories.name") ~
-      get[String]("poi_categories.image_path") map {
-      case id ~ name ~ image_path => PoiCategory(id, name, image_path)
-    }
+      get[Long]("poi_categories.id") ~
+          get[String]("poi_categories.name") ~
+          get[String]("poi_categories.image_path") map {
+            case id ~ name ~ image_path => PoiCategory(id, name, image_path)
+      }
   }
 
-  def getAllPois(): List[Poi] = {
-    return List[Poi]()
-  }
-
+  /**
+   * Find all Poi categories
+   *
+   * @return  List of the PoiCategory objects
+   */
   def findAll(): List[PoiCategory] = {
-
-    DB.withConnection("poi") {
-      implicit connection =>
-        SQL("select * from poi_categories").as(PoiCategory.simple *)
-    }
+      DB.withConnection("poi") { implicit connection =>
+          SQL(
+            """
+              |select * from poi_categories
+            """"
+          ).as(PoiCategory.simple *)
+      }
   }
 
+  /**
+   * Create a POi category
+   *
+   * @param poiCategory  new poi category object
+   * @return
+   */
   def create(poiCategory: PoiCategory) = {
-
-    DB.withConnection("poi") {
-      implicit connection =>
-        SQL( """
-        insert into poi_categories (name, image_path) values (
-        {name}, {image_path}
-        )
-             """
-        ).on(
-          'name -> poiCategory.name,
-          'image_path -> poiCategory.imagePath
-        ).executeInsert()
-
-        poiCategory
-
-    }
-
+      DB.withConnection("poi") { implicit connection =>
+          SQL(
+            """
+              |insert into poi_categories (name, image_path)
+              |values ({name}, {image_path})
+            """
+          ).on(
+              'name -> poiCategory.name,
+              'image_path -> poiCategory.imagePath
+          ).executeInsert()
+          poiCategory
+      }
   }
 }
 
