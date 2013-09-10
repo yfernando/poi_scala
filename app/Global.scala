bbee4f28ae67bda5c0172b40267b79e34de3a54b
yohan <yohanfernando@gmail.com>
2013-09-10 20:26:22 +0100

    updates as of 10th Sep 2013

--- app/Global.scala
+++ app/Global.scala
@@ -30,10 +30,35 @@ object InitialData {
 
 
       Seq(
-        PoiCategory(1,"Pub","some path"),
-        PoiCategory(2,"Hotel","some path"),
-        PoiCategory(3,"Airport","some path"),
-        PoiCategory(4,"Underground","some path")
+        PoiCategory(1,"art_gallery","some path"),
+        PoiCategory(2,"atm","some path"),
+        PoiCategory(3,"bakery","some path"),
+        PoiCategory(4,"bank","some path"),
+        PoiCategory(5,"bar","some path"),
+        PoiCategory(6,"beauty_salon","some path"),
+        PoiCategory(7,"bicycle_store","some path"),
+        PoiCategory(8,"book_store","some path"),
+        PoiCategory(9,"bowling_alley","some path"),
+        PoiCategory(10,"bus_station","some path"),
+        PoiCategory(11,"cafe","some path"),
+        PoiCategory(12,"campground","some path"),
+        PoiCategory(13,"car_dealer","some path"),
+        PoiCategory(14,"car_rental","some path"),
+        PoiCategory(15,"car_repair","some path")
+//        PoiCategory("car_wash","some path"),
+//        PoiCategory("casino","some path"),
+//        PoiCategory("cemetery","some path"),
+//        PoiCategory("church","some path"),
+//        PoiCategory("city_hall","some path"),
+//        PoiCategory("clothing_store","some path"),
+//        PoiCategory("convenience_store","some path"),
+//        PoiCategory("courthouse","some path"),
+//        PoiCategory("dentist","some path"),
+//        PoiCategory("department_store","some path"),
+//        PoiCategory("doctor","some path"),
+//        PoiCategory("electrician","some path"),
+//        PoiCategory("electronics_store","some path")
+
       ).foreach(PoiCategory.create)
 
     }
